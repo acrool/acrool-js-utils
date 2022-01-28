afee68ba2e72bc59d30217e7f8e469a4a12b9433
imagine10255 <imagine10255@gmail.com>
2022-01-28 15:47:50 +0800

    tmp save

--- example/src/index.ts
+++
@@ -1,7 +0,0 @@
-import {toCapitalize} from 'imagine-js-utils/format';
-// import {toCapitalize} from 'imagine-js-utils/format';
-import equal from 'deep-equal';
-
-console.log('test toCapitalize', toCapitalize('helloworld'));
-console.log('test equal', equal('helloworld','dasd'));
-
