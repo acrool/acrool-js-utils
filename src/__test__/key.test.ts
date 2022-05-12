a61a8c00413d041e9d48530dce8b7c0f2d3bcd68
imagine10255 <imagine10255@gmail.com>
2022-05-12 09:50:36 +0800

    feat: 新增 key uuid, timekey 加入前綴參數, 並固定timekey 長度

--- src/__test__/key.test.ts
+++ src/__test__/key.test.ts
@@ -2,8 +2,10 @@ import {timeKey, uuid} from '../key';
 
 test('uuid', () => {
   expect(uuid()).toHaveLength(36);
+  expect(uuid('game_')).toHaveLength(36 + 5);
 });
 
 test('timeKey', () => {
-  expect(timeKey()).toHaveLength(12);
+  expect(timeKey()).toHaveLength(15);
+  expect(timeKey('game_')).toHaveLength(15 + 5);
 });
--- src/key.ts
+++ src/key.ts
@@ -2,30 +2,39 @@
  * 產生UUID
  * @private
  */
-export function uuid(): string {
+import {paddingLeft} from './string';
+
+/**
+ * 產生36位UUID字串
+ * @param prefix
+ */
+export function uuid(prefix = ''): string {
     let d = Date.now();
     if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
         d += performance.now(); //use high-precision timer if available
     }
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const key = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         let r = (d + Math.random() * 16) % 16 | 0;
         d = Math.floor(d / 16);
         return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
     });
+
+    return `${prefix}${key}`;
 }
 
 /**
- * 產生 12位數日期時間不含西元年前兩碼
+ * 產生15位數日期時間(不含西元年前兩碼)
  */
-export function timeKey(): string {
+export function timeKey(prefix = ''): string {
     const d = new Date();
     const year = d.getFullYear().toString().substr(2,2);
-    const month = d.getMonth();
-    const day = d.getDay();
-    const hours = d.getHours();
-    const minutes = d.getMinutes();
-    const seconds = d.getSeconds();
-    const milliseconds = d.getMilliseconds();
+    const month = paddingLeft(d.getMonth() + 1, 2);
+    const day = paddingLeft(d.getDay(), 2);
+    const hours = paddingLeft(d.getHours(), 2);
+    const minutes = paddingLeft(d.getMinutes(), 2);
+    const seconds = paddingLeft(d.getSeconds(), 2);
+    const milliseconds = paddingLeft(d.getMilliseconds(), 2);
 
-    return [year, month, day, hours, minutes, seconds, milliseconds].join('');
+    const key = [year, month, day, hours, minutes, seconds, milliseconds].join('');
+    return `${prefix}${key}`;
 }
