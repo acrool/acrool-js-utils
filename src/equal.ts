b7869ca9e8517757961a2add0a1aa5d6ad5f5e44
imagine10255 <imagine10255@gmail.com>
2022-08-16 18:35:37 +0800

    feat: add regPattern svg, symbol, htmlAttrId in equal

--- src/equal.ts
+++ src/equal.ts
@@ -11,6 +11,9 @@ export const regPattern = {
     domain: /(:(http(s)?:\/\/.))?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}/g,
     ipAddress: /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/,
     date: /^([0-9]{4})[/.-]{1}([0-9]{1,2})[/.-]{1}([0-9]{1,2})$/,
+    svg: /<svg\b[^>]*?(?:viewBox=\"(\b[^"]*)\")?>([\s\S]*?)<\/svg>/g,
+    symbol: /<symbol\b[^>]*?(?:viewBox=\"(\b[^"]*)\")?>([\s\S]*?)<\/symbol>/g,
+    htmlAttrId: /id=\"(.*?)\"/g,
 };
 
 
