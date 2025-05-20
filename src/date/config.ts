68e607cd450a439bcf1a98eb62cedcca9d6a037d
imagine10255 <imagine10255@gmail.com>
2025-05-20 20:33:39 +0800

    Add support for extended date ranges and formatting options
    Introduced new utility functions and constants to handle additional date ranges, such as lastMonth, twoMonth, and thisYear. Defined a new format object for standardized date and time formats and added the IRangeDate interface to structure range-based date calculations.

--- src/date/config.ts
+++ src/date/config.ts
@@ -1,14 +1,14 @@
 import dayjs, {Dayjs} from 'dayjs';
 
-import {TDayList} from './types';
-
 const today = dayjs();
 const thisWeek = today.day(0);
 const lastWeek = thisWeek.subtract(7, 'day');
 const twoWeekAgo = thisWeek.subtract(14, 'day');
 const thisMonth = today.set('date', 1);
+const lastMonth = thisMonth.subtract(1, 'month');
+const twoMonth = thisMonth.subtract(2, 'month');
+const thisYear = today.set('month',0).set('date',1);
 const halfYearAgo = today.subtract(6, 'month');
-
 
 export const dateUtils = {
     today,
@@ -19,15 +19,33 @@ export const dateUtils = {
     halfYearAgo,
 };
 
+export const format = {
+    date: 'YYYY/MM/DD',
+    dateDash: 'YYYY-MM-DD',
+    dateTime: 'YYYY/MM/DD HH:mm:ss',
+    shortDateTime: 'YYYY/MM/DD HH:mm',
+    shortTime: 'HH:mm',
+    time: 'HH:mm:ss',
+};
 
 export enum EDayKey {
-    today= 'today',
+    today = 'today',
     yesterday = 'yesterday',
     thisWeek = 'thisWeek',
     lastWeek = 'lastWeek',
     twoWeekAgo = 'twoWeekAgo',
     thisMonth = 'thisMonth',
+    lastMonth = 'lastMonth',
+    twoMonth = 'twoMonth',
+    thisHalfYear = 'thisHalfYear',
+    thisYear = 'thisYear',
 }
+export interface IRangeDate {
+    startDate: Dayjs
+    endDate: Dayjs
+}
+
+export type TDayList = Record<EDayKey, IRangeDate>
 
 
 export const dayList: TDayList = {
@@ -37,4 +55,8 @@ export const dayList: TDayList = {
     [EDayKey.lastWeek]: {startDate: lastWeek, endDate: lastWeek.add(6, 'day')},
     [EDayKey.twoWeekAgo]: {startDate: twoWeekAgo, endDate: twoWeekAgo.add(6, 'day')},
     [EDayKey.thisMonth]: {startDate: thisMonth, endDate: thisMonth.endOf('month')},
+    [EDayKey.lastMonth]: {startDate: lastMonth, endDate: lastMonth.endOf('month')},
+    [EDayKey.twoMonth]: {startDate: twoMonth, endDate: twoMonth.endOf('month')},
+    [EDayKey.thisHalfYear]: {startDate: today.subtract(6, 'month'), endDate: today},
+    [EDayKey.thisYear]: {startDate: thisYear, endDate: thisYear.endOf('year')},
 };
